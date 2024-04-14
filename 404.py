6334eb0bb96819b50a173d9ba6cf8ac83768e083
AfsalAfzz-Pro <muhammedafsalafzz@gmail.com>
2024-04-14 14:11:37 +0530

    first commit

---
+++ 404.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root) -> int:
+        def sum_left_leaves(node):
+            if node is None:
+                return 0
+            
+            # Check if the left child is a leaf node
+            if node.left and node.left.left is None and node.left.right is None:
+                return node.left.val + sum_left_leaves(node.right)  # Add the value of left leaf and recursively search in the right subtree
+            else:
+                return sum_left_leaves(node.left) + sum_left_leaves(node.right)  # Recursively search in both subtrees
+        result = sum_left_leaves(root)
+        print("sum : ", result)
+        return result
